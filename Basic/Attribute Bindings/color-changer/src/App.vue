b69f7b3bbe4cf9e5f41a4c77ae2327356f86f0c3
DEVEV01 <everly.jang@gmail.com>
2023-02-02 09:41:45 +0900

    Create Vue app - Color Changer
    starting code

---
+++ Basic/Attribute Bindings/color-changer/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <ColorButton color="Red" />
+  <ColorButton color="Blue" />
+  <ColorButton color="Green" />
+</template>
+
+<script>
+import ColorButton from './components/ColorButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ColorButton
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Basic/Attribute Bindings/color-changer/src/components/ColorButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button>Turn me {{ color }}</button>
+</template>
+
+<script>
+export default {
+  name: 'ColorButton',
+  props: {
+    color: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
