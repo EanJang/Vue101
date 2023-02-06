1c3f2fbf0dc8942720677ad92ff67118aa01fd22
DEVEV01 <everly.jang@gmail.com>
2023-02-06 12:00:41 +0900

    Create Vue app - List Playground
    ...and add some code

---
+++ Basic/Conditionals/list-playground/src/components/MainPage.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="main">
+    <h1>{{ msg }}</h1>
+    <p>
+      Toggle, Push & Pop, and Reverse Your List!
+    </p>
+    <h3>Show & Hide</h3>
+    <button v-on:click="show = !show">SHOW</button>
+    <ul v-if="show">
+      <li v-for="(item, index) of list" v-bind:key="index">{{ item }}</li>
+    </ul>
+    <h3>Push & Pop</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Reverse</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      show: true,
+      list: [1, 2, 3]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  background: none;
+  border: none;
+  border-bottom: 1px solid #42b983;
+  color: #42b983;
+  cursor: pointer;
+  margin: 30px 0 5px 0;
+}
+</style>
