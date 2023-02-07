0a6818cd741cdb91a2cc88b522386ff51f99ac50
DEVEV01 <everly.jang@gmail.com>
2023-02-07 14:28:08 +0900

    Refactor ordered list
    remove bracket from array and replace comma with blank

--- Basic/Form Bindings/form-playground/src/components/MainPage.vue
+++ Basic/Form Bindings/form-playground/src/components/MainPage.vue
@@ -35,35 +35,35 @@
       <li>
         <label class="control control-checkbox" for="multi1">
           Cheese🧀
-          <input type="checkbox" id="multi1" value="🧀" v-model="orderedList" />
+          <input type="checkbox" id="multi1" value="🧀" v-model="multiChecked" />
           <div class="control_indicator"></div>
         </label>
       </li>
       <li>
         <label class="control control-checkbox" for="multi2">
           Chocolate🍫
-          <input type="checkbox" id="multi2" value="🍫" v-model="orderedList" />
+          <input type="checkbox" id="multi2" value="🍫" v-model="multiChecked" />
           <div class="control_indicator"></div>
         </label>
       </li>
       <li>
         <label class="control control-checkbox" for="multi3">
           Cake🍰
-          <input type="checkbox" id="multi3" value="🍰" v-model="orderedList" />
+          <input type="checkbox" id="multi3" value="🍰" v-model="multiChecked" />
           <div class="control_indicator"></div>
         </label>
       </li>
       <li>
         <label class="control control-checkbox" for="multi4">
           Caffè Latte☕
-          <input type="checkbox" id="multi4" value="☕" v-model="orderedList" />
+          <input type="checkbox" id="multi4" value="☕" v-model="multiChecked" />
           <div class="control_indicator"></div>
         </label>
       </li>
     </ul>
     <ul class="order-box">
       <p>I will order:</p>
-      <p>{{ orderedList }}</p>
+      <p class="order-list">{{ orderedList }}</p>
     </ul>
     <h3>Radio</h3>
     <ul>
@@ -93,13 +93,17 @@ export default {
       } else {
         return ''
       }
+    },
+    orderedList() {
+      const list = this.multiChecked.toString().replaceAll(",", " "); //remove bracket from array, and replace comma with blank
+      return list;
     }
   },
   data() {
     return {
       inputText: '',
       checked: false,
-      orderedList: [],
+      multiChecked: [],
     }
   },
 }
@@ -237,5 +241,9 @@ input[type=text]:focus {
 }
 .order-box {
   margin-top: 40px;
+}
+.order-list {
+  text-decoration: underline 1px solid black;
+  text-underline-offset: 4px;
 }
 </style>
