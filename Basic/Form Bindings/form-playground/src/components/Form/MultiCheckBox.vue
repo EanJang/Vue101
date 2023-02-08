4f2d51f3e65fabc6513c707e7076f2d914724828
DEVEV01 <everly.jang@gmail.com>
2023-02-08 16:24:22 +0900

    Extract Multi Checkbox component from main

---
+++ Basic/Form Bindings/form-playground/src/components/Form/MultiCheckBox.vue
@@ -0,0 +1,65 @@
+<template>
+<h3>Multi Checkbox</h3>
+    <ul>
+        <li>
+            <label class="control control-checkbox" for="multi1">
+                Cheese🧀
+            <input type="checkbox" id="multi1" value="🧀" v-model="multiChecked" />
+            <div class="control_indicator"></div>
+            </label>
+        </li>
+        <li>
+            <label class="control control-checkbox" for="multi2">
+                Chocolate🍫
+            <input type="checkbox" id="multi2" value="🍫" v-model="multiChecked" />
+            <div class="control_indicator"></div>
+            </label>
+        </li>
+        <li>
+            <label class="control control-checkbox" for="multi3">
+                Cake🍰
+            <input type="checkbox" id="multi3" value="🍰" v-model="multiChecked" />
+            <div class="control_indicator"></div>
+            </label>
+        </li>
+        <li>
+            <label class="control control-checkbox" for="multi4">
+                Caffè Latte☕
+            <input type="checkbox" id="multi4" value="☕" v-model="multiChecked" />
+            <div class="control_indicator"></div>
+            </label>
+        </li>
+    </ul>
+    <ul class="order-box">
+        <p>I will order:</p>
+        <p class="order-list">{{ orderedList }}</p>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'multi-check-box',
+        computed: {
+            orderedList() {
+                const list = this.multiChecked.toString().replaceAll(",", " "); //remove bracket from array, and replace comma with blank
+                return list;
+            }
+        },
+        data() {
+            return {
+                multiChecked: []
+            }
+        },
+    }
+</script>
+
+<style>
+.order-box {
+    margin-top: 40px;
+    margin-bottom: 55px;
+}
+.order-list {
+    text-decoration: underline 1px solid black;
+    text-underline-offset: 4px;
+}
+</style>
--- Basic/Form Bindings/form-playground/src/components/MainPage.vue
+++ Basic/Form Bindings/form-playground/src/components/MainPage.vue
@@ -6,41 +6,7 @@
     </p>
     <TextInput />
     <CheckBox />
-    <h3>Multi Checkbox</h3>
-    <ul>
-      <li>
-        <label class="control control-checkbox" for="multi1">
-          Cheese🧀
-          <input type="checkbox" id="multi1" value="🧀" v-model="multiChecked" />
-          <div class="control_indicator"></div>
-        </label>
-      </li>
-      <li>
-        <label class="control control-checkbox" for="multi2">
-          Chocolate🍫
-          <input type="checkbox" id="multi2" value="🍫" v-model="multiChecked" />
-          <div class="control_indicator"></div>
-        </label>
-      </li>
-      <li>
-        <label class="control control-checkbox" for="multi3">
-          Cake🍰
-          <input type="checkbox" id="multi3" value="🍰" v-model="multiChecked" />
-          <div class="control_indicator"></div>
-        </label>
-      </li>
-      <li>
-        <label class="control control-checkbox" for="multi4">
-          Caffè Latte☕
-          <input type="checkbox" id="multi4" value="☕" v-model="multiChecked" />
-          <div class="control_indicator"></div>
-        </label>
-      </li>
-    </ul>
-    <ul class="order-box">
-      <p>I will order:</p>
-      <p class="order-list">{{ orderedList }}</p>
-    </ul>
+    <MultiCheckBox />
     <h3>Radio</h3>
     <ul>
       <li>
@@ -106,12 +72,14 @@
 <script>
 import TextInput from './Form/TextInput'
 import CheckBox from './Form/CheckBox'
+import MultiCheckBox from './Form/MultiCheckBox'
 
 export default {
   name: 'MainPage',
   components: {
     TextInput,
-    CheckBox
+    CheckBox,
+    MultiCheckBox
   },
   props: {
     msg: String
@@ -344,14 +312,6 @@ li {
 }
 .checked {
   text-decoration: underline 1px solid #42b983;
-}
-.order-box {
-  margin-top: 40px;
-  margin-bottom: 55px;
-}
-.order-list {
-  text-decoration: underline 1px solid black;
-  text-underline-offset: 4px;
 }
 select {
   font-size: 13px;
