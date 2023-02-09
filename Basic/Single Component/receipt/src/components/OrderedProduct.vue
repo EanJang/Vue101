a093d85a8ca9cce6ecaec538a041bc25c1598743
DEVEV01 <everly.jang@gmail.com>
2023-02-09 17:15:54 +0900

    Extract order detail component from Receipt

---
+++ Basic/Single Component/receipt/src/components/OrderedProduct.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="details">
+        <h3> Details </h3>
+        <div class="product">
+            <img src="https://www.freeiconspng.com/uploads/lunch-panini-sandwiches-25.png" alt="">
+            <div class="info">
+                <h4> Panini Sandwich </h4>
+                <p> Tomato & Mozzarella </p>
+                <p> Qty: 1 </p>
+            </div>
+        </div>
+        <p> 9 &dollar; </p>
+    </div>
+
+    <div class="details">
+        <div class="product">
+            <img src="https://www.freeiconspng.com/uploads/lunch-png-8.png" alt="">
+            <div class="info">
+                <h4> Fresh Lunch Set </h4>
+                <p> Turkey Bacon </p>
+                <p> Qty: 1 </p>
+            </div>
+        </div>
+        <p> 14 &dollar; </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'OrderedProduct'
+    }
+</script>
+
+<style>
+
+</style>
--- Basic/Single Component/receipt/src/components/ReceiptMain.vue
+++ Basic/Single Component/receipt/src/components/ReceiptMain.vue
@@ -13,37 +13,7 @@
 <hr>
 
 <!-- Details -->
-<div class="details">
-
-<h3> Details </h3>
-<div class="product">
-    <img src="https://www.freeiconspng.com/uploads/lunch-panini-sandwiches-25.png" alt="">
-    <div class="info">
-      <h4> Panini Sandwich </h4>
-      <p> Tomato & Mozzarella </p>
-      <p> Qty: 1 </p>
-    </div>
-  </div>
-
-  <p> 9 &dollar; </p>
-
-</div>
-
-<!-- Details -->
-<div class="details">
-
-<div class="product">
-    <img src="https://www.freeiconspng.com/uploads/lunch-png-8.png" alt="">
-    <div class="info">
-      <h4> Fresh Lunch Set </h4>
-      <p> Turkey Bacon </p>
-      <p> Qty: 1 </p>
-    </div>
-  </div>
-
-  <p> 14 &dollar; </p>
-
-</div>
+<OrderedProduct />
 
 <!-- Sub and total price -->
 <div class="totalprice">
@@ -60,8 +30,13 @@
 </template>
 
 <script>
+import OrderedProduct from './OrderedProduct'
+
 export default {
-  name: 'receipt-main'
+  name: 'receipt-main',
+  components: {
+    OrderedProduct
+  }
 }
 </script>
 
