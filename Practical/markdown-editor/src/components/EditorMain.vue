1dda4b44112f801d527e6a29b00615b569df708e
DEVEV01 <klaude.jang@gmail.com>
2023-02-14 09:46:04 +0900

    Clean up starting code

---
+++ Practical/markdown-editor/src/components/EditorMain.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="editor">
+    <h1>{{ msg }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditorMain',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
