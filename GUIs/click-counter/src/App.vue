5655ead9558fd232a5097a99bbb0d23ea3529c1f
DEVEV01 <klaude.jang@gmail.com>
2023-02-16 09:08:44 +0900

    Init project: click counter

---
+++ GUIs/click-counter/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ClickCounter title="Click Counter"/>
+  </div>
+</template>
+
+<script>
+import ClickCounter from './components/ClickCounter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ClickCounter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
