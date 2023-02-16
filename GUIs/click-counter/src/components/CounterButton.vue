952fad0b116565bff419719fdbc58228e3fcad61
DEVEV01 <klaude.jang@gmail.com>
2023-02-16 14:27:20 +0900

    Add Counter Button component: - and +

---
+++ GUIs/click-counter/src/components/CounterButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button>{{ symbol }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CounterButton',
+    props: {
+        symbol: String
+    },
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
