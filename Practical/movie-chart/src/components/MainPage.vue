f8f9675c6069b7349df2b4f2b483fcc3b2dfeac5
LauJang <klaude.jang@gmail.com>
2023-02-20 09:36:44 +0900

    Create Vue App - movie chart
    with starting code

---
+++ Practical/movie-chart/src/components/MainPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="main">
+    <h1>{{ msg }}</h1>
+    <h3>
+      Weekend US Box Office Chart for February 17, 2023
+    </h3>
+    <p>
+      Visit  <a href="https://www.the-numbers.com/weekend-box-office-chart" target="_blank">THE NUMBERS</a> web page for more details.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
